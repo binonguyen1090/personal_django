d69dbfe2090199f4b32f884ac0d22d0791641031
Bino <binonguyen1090@gmail.com>
2019-11-15 12:59:07 -0600

    Create Form Bootstrap and submit them to database

--- personal/helloworld/views.py
+++ personal/helloworld/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Contact
 def index(request):
     return render(request, 'helloworld/index.html')
 
@@ -7,4 +7,13 @@ def portfolio(request):
     return render(request, 'helloworld/portfolio.html')
 
 def contact(request):
-    return render(request, 'helloworld/contact.html')
+    if request.method == "POST":
+        email = (request.POST.get('email'))
+        subject = (request.POST.get('subject'))
+        message = (request.POST.get('message'))
+
+        c = Contact(email=email,subject=subject,message=message)
+        c.save()
+        return render(request, 'helloworld/thank.html')
+    else:
+        return render(request, 'helloworld/contact.html')
