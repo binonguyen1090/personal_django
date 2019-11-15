5338648e2edd5598de8a7c1c52feb042cbb41546
Bino <binonguyen1090@gmail.com>
2019-11-15 10:27:31 -0600

    Add app.urls to point separate app views

--- personal/personal/urls.py
+++ personal/personal/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from helloworld.views import *
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello', index),
+    path('', include('helloworld.urls')),
 ]
