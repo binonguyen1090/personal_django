1ae13163639d40156960bec57584bce550654cdf
Bino <binonguyen1090@gmail.com>
2019-11-15 11:48:58 -0600

    add dynamic web response

--- personal/helloworld/urls.py
+++ personal/helloworld/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
+    path('portfolio', views.portfolio, name='portfolio'),
+    path('contact', views.contact, name='contact')
 ]
--- personal/helloworld/views.py
+++ personal/helloworld/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'helloworld/index.html')
+
+def portfolio(request):
+    return render(request, 'helloworld/portfolio.html')
+
+def contact(request):
+    return render(request, 'helloworld/contact.html')
