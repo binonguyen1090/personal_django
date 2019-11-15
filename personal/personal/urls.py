60c4b6d076ef9469427eab49e5657b9c85ee1bd2
Bino <binonguyen1090@gmail.com>
2019-11-15 10:14:00 -0600

    Install app with 'hello world' print out at /hello

--- personal/personal/urls.py
+++ personal/personal/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from helloworld.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello', index),
 ]
